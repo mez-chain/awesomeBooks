444d1d33f744b1b82c33639416002ca4a6a647c0
N.MEZIANY <mez.blockchain@gmail.com>
2024-07-29 19:46:50 +0300

    added the js functions to add new books and show the books in the page

---
+++ js/main.js
@@ -0,0 +1,50 @@
+let collection = JSON.parse(localStorage.getItem("collection")) || [];
+const btnAdd = document.getElementById("addButton");
+
+btnAdd.addEventListener("click", addBook);
+
+//Show books list
+function showBooks() {
+  const bookList = document.getElementById("awesomeList");
+  bookList.innerHTML = "";
+
+  collection.forEach((book, index) => {
+    const bookItem = document.createElement("div");
+    bookItem.classList = "book";
+    bookItem.innerHTML = `
+    <p>${book.title}</p>
+    <p>${book.author}</p>
+    <button class="removeBtn" onclick="removeBook(${index})">Remove</button>
+    `;
+
+    bookList.appendChild(bookItem);
+  });
+}
+
+//Add a new book to the collection
+function addBook() {
+  const title = document.getElementById("title").value;
+  const author = document.getElementById("author").value;
+  if (title && author) {
+    const newBook = { title, author };
+    collection.push(newBook);
+
+    localStorage.setItem("collection", JSON.stringify(collection));
+
+    document.getElementById("title").value = "";
+    document.getElementById("author").value = "";
+
+    showBooks();
+  }
+}
+
+//Remove a book from the collection
+function removeBook(index) {
+  collection.splice(index, 1);
+
+  localStorage.setItem("collection", JSON.stringify(collection));
+
+  showBooks();
+}
+
+showBooks();
