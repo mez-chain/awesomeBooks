4afc385eb6091b846f7e6f65a92ea47532773346
N.MEZIANY <mez.blockchain@gmail.com>
2024-08-07 04:36:38 +0300

    added validation using js for the form

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 const collection = JSON.parse(localStorage.getItem('collection')) || [];
 const btnAdd = document.getElementById('addButton');
+const errorMsg = document.getElementById("error-msg");
+
 
 class Books {
   constructor(title, author) {
@@ -11,6 +13,10 @@ class Books {
   addBook() {
     const title = document.getElementById('title').value;
     const author = document.getElementById('author').value;
+
+    // Form Validation
+    validateForm(title, author);
+
     if (title && author) {
       const newBook = new Books(title, author);
       collection.push(newBook);
@@ -55,3 +61,41 @@ function showBooks() {
 
 btnAdd.addEventListener('click', () => newBookInstance.addBook());
 showBooks();
+
+// Form Validation
+function validateForm(title, author) {
+  errorMsg.innerHTML = "";
+  errorMsg.classList.remove("error");
+  document.getElementById('title').classList.remove("error-input");
+  document.getElementById('author').classList.remove("error-input");
+
+  if (title === "" && author !== "") {
+    errorMsg.innerHTML = "Please enter the book's title";
+    errorMsg.classList.add("error");
+    document.getElementById('title').classList.add("error-input");
+  } else if (author === "" && title !== "") {
+    errorMsg.innerHTML = "Please enter the book's author"
+    errorMsg.classList.add("error");
+    document.getElementById('author').classList.add("error-input");
+  } else if ( title === "" && author === "") {
+    errorMsg.innerHTML = "Please enter the book's title and author"
+    errorMsg.classList.add("error");
+    document.getElementById('title').classList.add("error-input");
+    document.getElementById('author').classList.add("error-input");
+  } else {
+    errorMsg.innerHTML = "A new book added successfully"
+    errorMsg.classList.add("success");
+ }
+}
+
+document.getElementById('title').addEventListener("focus", ( ) => {
+  errorMsg.innerHTML = "";
+  errorMsg.classList.remove("error");
+  document.getElementById('title').classList.remove("error-input");
+})
+
+document.getElementById('author').addEventListener("focus", ( ) => {
+  errorMsg.innerHTML = "";
+  errorMsg.classList.remove("error");
+  document.getElementById('author').classList.remove("error-input");
+})
